c2f894e4c2ca9f48991caccd3238152ea2135ee8
Arsen Shkrumelyak <storm@nerverift.com>
2022-12-15 17:56:48 +0000

    Debugging Tool

---
+++ Debug/Debug.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DevRIFT\Debug;
+
+use DevRIFT\DevRIFT;
+use DevRIFT\Exception;
+use DevRIFT\ApiRequestor;
+
+class Debug
+{
+    /**
+     * Check if the API keys provided are valid.
+     * 
+     * WARNING: Do not use this function in production.
+     * 
+     * @throws Exception\DebugFailureException
+     * @throws Exception\DebugSuccessException
+     * 
+     * Note: This function is only for debugging purposes.
+     * This function is likely to be removed or changed in the future.
+     */
+    public static function keys()
+    {
+        $keys = DevRIFT::getApiKeys();
+        if (!isset($keys['rift_pk']) || !isset($keys['rift_sk'])) {
+            throw new Exception\DebugFailureException("Keys were not provided");
+        } else {
+            array_merge(
+                $keys,
+                array(
+                    'api-type' => 'debug_keys'
+                )
+            );
+            if ((new ApiRequestor())->request($keys)) {
+                throw new Exception\DebugSuccessException("Keys are valid");
+            } else {
+                throw new Exception\DebugFailureException("Keys are invalid");
+            }
+        }
+    }
+}
