b05e35ed5ff40ee4c09522a2b90f5b2604c85618
Arsen Shkrumelyak <storm@nerverift.com>
2022-12-15 17:55:54 +0000

    Activation System
    https://docs.devrift.co/authentication/activation-mail

---
+++ Activation/Request.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DevRIFT\Activation;
+
+use DevRIFT\DevRIFT;
+use DevRIFT\Exception;
+
+class Activation
+{
+    private static $publisherKey = null;
+    private static $secretKey = null;
+
+    public function __construct()
+    {
+        self::$publisherKey = DevRIFT::getPublisherKey();
+        self::$secretKey = DevRIFT::getSecretKey();
+    }
+
+    public static function request($email)
+    {
+        // Check if the email is valid
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception\InvalidArgumentException("Invalid email address");
+        }
+
+        // Create a data array with the GET parameters and email
+        $data = array(
+            'api-type' => 'activation_create',
+            'email' => $email,
+            'rift_pk' => self::$publisherKey,
+            'rift_sk' => self::$secretKey
+        );
+
+        // Use the ApiRequestor class to send the data to the API
+        return (new \DevRIFT\ApiRequestor())->request($data);
+    }
+}
